de059acb3f4ee30012028ab119c5c9e5319ca8a8
boska <mr.boska@gmail.com>
2017-05-23 14:43:15 +0800

    remove Pods folder

--- Example/Pods/Target Support Files/Pods-ViscoveryADSDK_Example/Pods-ViscoveryADSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ViscoveryADSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ViscoveryADSDK_Example
-@end
--- Example/Pods/Target Support Files/ViscoveryADSDK/ViscoveryADSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ViscoveryADSDK : NSObject
-@end
-@implementation PodsDummy_ViscoveryADSDK
-@end
